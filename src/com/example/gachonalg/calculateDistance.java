8e0d6cf674177881bcb457961e705072b35cba3e
sumin13245 <lili1207@naver.com>
2021-11-24 00:54:23 +0900

    QueryAsnum, timetable 에 넣는것 추가 clossedistance일단 계획만 작성

--- src/com/example/gachonalg/calculateDistance.java
+++
@@ -1,6 +0,0 @@
-package com.example.gachonalg;
-
-public class calculateDistance {
-    // 시작 수업부터 거리를 가까운것만 집어넣기
-
-}
--- src/com/example/gachonalg/mainClass.java
+++ src/com/example/gachonalg/mainClass.java
@@ -11,7 +11,7 @@ public class mainClass {
         BufferedReader fileReader = new BufferedReader(file_input);
 
         String data;//lessonName, professorName, date, building,building_num, lesson_num;
-        int[] userWant = new int[3];
+        int userWant;
         timeTable inputTimeTable = new timeTable();
 
         lesson[] lessonData = new lesson[200]; // 데이터 배열로 담았어요 (매번 io 하긴 느릴 것 같아서)
@@ -26,16 +26,39 @@ public class mainClass {
         fileReader.close();
 
         Scanner input = new Scanner(System.in);
+
         int j = 0;
         while ( j < 3) {
-            userWant[j] = input.nextInt();
-            //여기에 그 파일에서 학수번호로 lesson 찾아서 inputTimeTable에 넣는 코드 구현하시면 돼요. addLesson 하면 True,False 나오니까 true 나올 때까지 받게 하면 중복피할 수 있음
-            j++;
+            int key;
+            userWant = input.nextInt();
+            key = queryAsNum(userWant,lessonData,i);
+            if(key!= -1)
+                if(inputTimeTable.addLesson(lessonData[key]));
+                    j++;
+                    //여기에 그 파일에서 학수번호로 lesson 찾아서 inputTimes 나오니까 true 나올 때까지 받게 하면 중복피할 수 있음(완료)
         }
 
         input.close();
-    }
+    } // main 함수의 끝 부분 괄호
 
+    private  timeTable closeDistance(timeTable inputTT,int max,lesson[] lessonsData){
+
+        // 먼저 건물이 4 개니 각각의 건물을 기준으로 정렬한 레슨데이터 4개를 생성,
+        //가장 가까운 건물의 경우 층수도 가장 가까운 것을 기준으로 정렬하고 다른 건물은 1층부터 정렬한다
+
+
+            // 정렬된  lesson data 중에서 시간이 가까운걸 위주로 add lesson 한다
+            //먼저 userwant 전후의 수업을 이어붙인다
+            // 이어붙이면 it 대학 수업의 경우 수업을 이틀 해서 다른 날도 길이 생김 그럼 거기에도 이어붙임
+
+            // 수업이 없는 날이 나온다면 다음 데이터를  이미 있는 수업과 다른 수업데이터를 하나씩 집어넣는다 (분배를 위해 다른 수업)
+            // 이제 기존 데이터를 기준으로 가까운 데이터를 집어넣어야 하는데  만약에 user want 가 같은날 여러개면
+            // 시간대가 이후인 userwant 이전인 시간 까지만 데이터를 집어넣고 이후에 또 이어붙여야 한다
+
+
+
+        return inputTT;
+    }
     //method daysOff with input timetable and max number of lessons and lessonData array
     private timeTable daysOff(timeTable inputTT, int max, lesson[] lessonsData) {
         // add maximum lesson when same days.
@@ -85,7 +108,14 @@ public class mainClass {
             }
         }
     }
-
+    private static int queryAsNum(int userWant,lesson[] lessonData,int dataSize){
+        for(int k = 0;k<dataSize;k++) {
+            if (lessonData[k].getLesson_num() == userWant)
+                return k;
+        }
+        System.out.println("Cannot find lesson");
+        return  -1;
+    }
 }
 /*데이터 set
 인터페이스
