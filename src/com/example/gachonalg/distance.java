5753f6fb178752c421a5a7ae9dc763ee7499281c
kjk7212 <54500840+kjk7212@users.noreply.github.com>
2021-11-29 21:08:11 +0900

    Update distance.java
    지하가 섞였을 경우 버그 수정

--- src/com/example/gachonalg/distance.java
+++ src/com/example/gachonalg/distance.java
@@ -6,7 +6,7 @@ public class distance {
 
     public double getDistance(String src, String des,double srcFloor, double desFloor){
         double distance;
-        double floorPoint = srcFloor/1000+desFloor/1000; // The number of floors in a different building
+        double floorPoint =  abs(srcFloor)/1000+ abs(desFloor)/1000; // The number of floors in a different building
         double sbFloorPoint = abs(srcFloor - desFloor) / 1000;// Number of floors in the same building
         switch (src){
             case "비전타워": // Starting point
